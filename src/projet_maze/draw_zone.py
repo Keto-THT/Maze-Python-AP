f43b1fc2ce217d99254495d8eb93ff40ccf98c25
eliottdauresbouvet <eliottdaubou44@gmail.com>
2025-12-16 17:01:23 +0100

    draw zone

---
+++ src/projet_maze/draw_zone.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+from maze_gen import MazeGenerator
+
+class MazeZone:
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+
+    def draw(self, screen, color):
+        pygame.draw.rect(screen, color, self.rect)
+
+
+
+
+    
