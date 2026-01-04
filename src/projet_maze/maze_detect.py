c754a85b15e10786b65d46258328fabd1be3e537
Maxence Le Cheualier <maxence.le_cheualier@etu.minesparis.psl.eu>
2026-01-04 16:54:22 +0100

    Ajout de la detection des zones en légenddant avec les lettres

--- src/projet_maze/maze_detect.py
+++ src/projet_maze/maze_detect.py
@@ -10,10 +10,10 @@ class DetectZone :
     def is_empty(self, x, y): 
         return self.grid[x][y] != '#'
 
-    def _detect_zones(self):
-        visited = [[False for i in range(self.width)] for j in range(sekf.height)]
-        for x in range(self.width): 
-            for y in range(self.height):
+    def detect_zones(self):
+        visited = [[False for i in range(self.width)] for j in range(self.height)]
+        for x in range(self.height): 
+            for y in range(self.width):
                 if self.is_empty(x, y) and not visited[x][y]:
                     zone = []
                     self._explore_zone(x, y, visited, zone)
@@ -24,12 +24,28 @@ class DetectZone :
         zone.append((x,y))
         for dx, dy in [(-1,0), (1,0), (0, -1), (0,1)]:
             i, j = x + dx, y + dy
-            if 0 <= i < self.width and 0 <= j < self.height : 
+            if 0 <= i < self.height and 0 <= j < self.width : 
                 if self.is_empty(i,j) and not visited[i][j]:
-                    self._explore(i,j,visited,zone)
+                    self._explore_zone(i,j,visited,zone)
     
     def print_zones(self):
         for index, zone in enumerate(self.zones):
-            print(f"Zone {idx + 1} : {zone}")
+            print(f"Zone {index + 1} : {zone}")
             for x, y in zone: 
                 print(f"({x}, {y})")
+    
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    def label_zones(self):
+        labeled_grid = [line.copy() for line in self.grid]
+        for index, zone in enumerate(self.zones):
+            letter = alphabet[index % len(alphabet)]
+            for x, y in zone :
+                labeled_grid[x][y] = letter
+        return labeled_grid
+    
+    def print_labeled_zones(self):
+        labeled_grid = self.label_zones()
+        for line in labeled_grid: 
+            print(''.join(line))
+        
