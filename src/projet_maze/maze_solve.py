7e3e62f18d93c0175e6a37b10b6cce3fac37bd5c
alexis-lauvergne <alexislauvergne@gmail.com>
2026-01-06 16:31:20 +0100

    Alexis Classe MazeSolve (#16)
    * Fonction solve v1
    
    * Maze_solve v2
    
    * Fonction Solve v3
    
    * Modification MazeSolve

--- src/projet_maze/maze_solve.py
+++ src/projet_maze/maze_solve.py
@@ -1,35 +1,55 @@
-class MazeSolve ():
-    def _init__ (self, maze):
+class MazeSolve:
+    def __init__(self, maze):  # Correction : double underscore
         self.maze = maze
-        self.enter = self.find_enter(maze)
-        self.exit = self.find_exit(maze)
+        self.enter = self.find_enter()
+        self.exit = self.find_exit()
 
     def maze_solve(self):
         enter, exit = self.enter, self.exit
-        chemin = {(enter[0]+1, enter[1]): [(enter[0], enter[1]), (enter[0]+1, enter[1])]}
+        # On commence le chemin à l'entrée même
+        chemin = {enter: [enter]}
         chemin_check = {}
+        
         while exit not in chemin:
             if chemin_check == chemin:
-                raise Exception ("Pas de solution trouvée à ce labyrinthe.")
+                raise Exception("Pas de solution trouvée à ce labyrinthe.")
+            
             chemin_check = chemin.copy()
             new_chemin = {}
+            
             for case in chemin:
-                voisins = [(case[0]+1, case[1]), (case[0]-1, case[1]), (case[0], case[1]+1), (case[0], case[1]-1)]
+                voisins = [(case[0]+1, case[1]), (case[0]-1, case[1]), 
+                           (case[0], case[1]+1), (case[0], case[1]-1)]
+                
                 for voisin in voisins:
-                    if maze[voisin[0]][voisin[1]] == ' ' and voisin not in chemin and voisin not in new_chemin:
-                        new_chemin[voisin] = chemin[case] + [voisin]
+                    # Vérification des limites du labyrinthe pour éviter les IndexError
+                    if 0 <= voisin[0] < len(self.maze) and 0 <= voisin[1] < len(self.maze[0]):
+                        if self.maze[voisin[0]][voisin[1]] == ' ' and voisin not in chemin and voisin not in new_chemin:
+                            new_chemin[voisin] = chemin[case] + [voisin]
+            
             chemin.update(new_chemin)
 
         return chemin[exit]
 
+    def find_enter(self):
+        # Cherche l'entrée sur la première ligne
+        for i in range(len(self.maze[0])):
+            if self.maze[0][i] == ' ':
+                return (0, i)
 
-    def find_enter (self, self.maze):
-        for i in range(len(maze[0])):
-            if maze[0][i] == ' ':
-                return (0,i)
-
-    def find_exit (self):
+    def find_exit(self):
+        # Cherche la sortie sur la dernière ligne
         for i in range(len(self.maze[-1])):
-            if maze[-1][i] == ' ':
-                return (len(self.maze)-1,i)
-            
+            if self.maze[-1][i] == ' ':
+                return (len(self.maze)-1, i)
+
+def load_maze(file_path):
+    with open(file_path, 'r') as f:
+        return [list(line.strip('\n')) for line in f.readlines()]
+
+def test_maze_solver():
+    mon_labyrinthe = load_maze('maze.txt')
+    solveur = MazeSolve(mon_labyrinthe)
+    solution = solveur.maze_solve()
+    return("Chemin trouvé :", solution)
+
